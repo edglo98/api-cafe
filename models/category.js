736f30819623370cc19edc53fedd4eafc540633b
Edgar Lopez <edgarlopezmgs@live.com>
2021-09-27 23:32:49 -0500

    added category schema

---
+++ models/category.js
@@ -0,0 +1,21 @@
+import pkg from 'mongoose'
+const { Schema, model } = pkg
+
+const CategorySchema = Schema({
+  name: {
+    type: 'string',
+    require: [true, 'El nombre es obligatorio']
+  },
+  status: {
+    type: Boolean,
+    default: true,
+    require: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    require: true
+  }
+})
+
+export default model('Category', CategorySchema)
--- routes/categories.js
+++ routes/categories.js
@@ -5,7 +5,26 @@ import { validateReq } from '../middlewares/validateReq.js'
 const routerCategory = Router()
 
 routerCategory.get('/', (req, res) => {
-  console.log('ok ok ')
+  res.json('ok ok')
+})
+
+routerCategory.get('/:id', (req, res) => {
+  res.json('ok ok')
+})
+
+// privado con cualquier persona con token
+routerCategory.post('/', (req, res) => {
+  res.json('ok ok')
+})
+
+// actualizar privado
+routerCategory.put('/:id', (req, res) => {
+  res.json('ok ok')
+})
+
+// actualizar privado
+routerCategory.delete('/:id', (req, res) => {
+  res.json('ok ok')
 })
 
 export default routerCategory
