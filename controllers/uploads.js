196d0552d31e4312480eb05c8980c6cfd09a5ea0
Edgar Lopez <edgarlopezmgs@live.com>
2021-09-30 23:20:21 -0500

    finish upload for colections

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,4 +1,5 @@
 import { response } from 'express'
+import fs from 'fs'
 import { uploadFile } from '../helpers/uploadFile.js'
 import User from '../models/user.js'
 import Product from '../models/product.js'
@@ -45,9 +46,17 @@ export const updateAndLoadFile = async (req, res = response) => {
         msg: 'No hay validacion de modelo, error interno'
       })
   }
-  const nombre = await uploadFile(req.files, undefined, colection)
-  model.image = colection + '/' + nombre
 
+  if (model.image) {
+    const { pathname: pathImage } = new URL(`../uploads/${colection}/${model.image}`, import.meta.url)
+
+    if (fs.existsSync(pathImage)) {
+      fs.unlinkSync(pathImage)
+    }
+  }
+
+  const fileName = await uploadFile(req.files, undefined, colection)
+  model.image = fileName
   model.save()
 
   res.json(model)
